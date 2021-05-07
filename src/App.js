f0ed5aa41f35fd558bde53c98250a3579ed16187
Viviann Rydh <rydh.vivian@gmail.com>
2021-05-07 23:12:02 +0200

    deploy my react app to github pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>This is my first deploied App</h1> 
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum illo fuga voluptatem commodi cum. Asperiores perspiciatis cum id odio quos fugiat eaque, possimus vero officia culpa, illum numquam atque itaque.</p>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Praesentium officiis vitae tenetur sed asperiores hic nihil, repudiandae repellat facere! Doloremque, nostrum quas at provident dolorem velit corporis quae vel accusamus.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus vel, sunt, quisquam consequatur perferendis quidem aspernatur nihil dolore nulla cum aperiam possimus reiciendis illum iste perspiciatis quia vero dignissimos saepe!</p>
     </div>
   );
 }
